6defc3ce5b7d6060ecc54cc3f6092c2bcf9fdd01
soander <Wang-yaozheng@outlook.com>
2022-11-05 23:34:41 -0700

    A8 assignment about history activity.

--- app/src/main/java/edu/northeastern/cs5520_group9/firebase/HistoryActivity.java
+++ app/src/main/java/edu/northeastern/cs5520_group9/firebase/HistoryActivity.java
@@ -3,14 +3,66 @@ package edu.northeastern.cs5520_group9.firebase;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 import edu.northeastern.cs5520_group9.R;
 
 public class HistoryActivity extends AppCompatActivity {
+    private List<Sticker> stickers;
+    private String userName;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        // Set the userName, databaseReference and stickers
+        userName = getIntent().getExtras().getString("user_name");
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        stickers = new ArrayList<>();
+        // Query from database and update history
+        update();
+        queryFromDatabase();
+    }
+
+    /**
+     * Update the history layout
+     */
+    private void update() {
+        RecyclerView received = findViewById(R.id.receivedHistoryRV);
+        received.setLayoutManager(new LinearLayoutManager(this));
+        received.setAdapter(new StickerAdapterRecyclerView(stickers));
+    }
+
+    /**
+     * Query the history from firebase database and update the layout
+     */
+    private void queryFromDatabase() {
+        stickers = new ArrayList<>();
+        databaseReference.child("stickers").get().addOnCompleteListener((t) -> {
+            HashMap<String, HashMap<String, String>> tempMap = (HashMap) t.getResult().getValue();
+            if (tempMap == null) return;
+            for (String Key : tempMap.keySet()) {
+                String fromUser = Objects.requireNonNull(tempMap.get(Key)).get("fromUser");
+                String id = String.valueOf(Objects.requireNonNull(tempMap.get(Key)).get("id"));
+                String sendTime = Objects.requireNonNull(tempMap.get(Key)).get("sendTime");
+                String toUser = Objects.requireNonNull(tempMap.get(Key)).get("toUser");
+                if (toUser != null && toUser.equals(userName)) {
+                    stickers.add(new Sticker(Integer.parseInt(id), fromUser, toUser, sendTime));
+                }
+            }
+            stickers.sort(Collections.reverseOrder());
+            update();
+        });
     }
 }
