8e9c677c5096ede15590d1a42d3ab7190c97a2d7
SophiaFan <fan.shou@northeastern.edu>
2022-11-08 15:01:17 -0800

    A8: update layout color

--- app/src/main/java/edu/northeastern/cs5520_group9/firebase/StickerViewHolderRecyclerView.java
+++ app/src/main/java/edu/northeastern/cs5520_group9/firebase/StickerViewHolderRecyclerView.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import edu.northeastern.cs5520_group9.R;
@@ -16,9 +18,9 @@ import edu.northeastern.cs5520_group9.R;
  */
 
 public class StickerViewHolderRecyclerView extends RecyclerView.ViewHolder {
-    private final ImageView receivedStickerView;
-    private final TextView fromUserView;
-    private final TextView sendTimeView;
+    final ImageView receivedStickerView;
+    final TextView fromUserView;
+    final TextView sendTimeView;
 
     public StickerViewHolderRecyclerView(@NonNull View itemView) {
         super(itemView);
@@ -36,7 +38,8 @@ public class StickerViewHolderRecyclerView extends RecyclerView.ViewHolder {
             receivedStickerView.setImageResource(imageResource);
         }
         fromUserView.setText(sticker.getFromUser());
-        sendTimeView.setText(new Date(sticker.getSendTimeEpochSecond()).toString());
+        DateFormat df = new SimpleDateFormat("dd MMM yyyy hh:mm:ss zzz");
+        sendTimeView.setText(df.format(new Date(sticker.getSendTimeEpochSecond())));
     }
 
     // to get sticker by id
