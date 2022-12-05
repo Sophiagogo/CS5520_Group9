98c0f75cf97bb7f879c7c1a984483d64f3aa82a6
soander <Wang-yaozheng@outlook.com>
2022-12-05 00:13:30 -0800

    Add sounds.

--- app/src/main/java/edu/northeastern/cs5520_group9/groupProject/Sound.java
+++ app/src/main/java/edu/northeastern/cs5520_group9/groupProject/Sound.java
@@ -1,49 +1,56 @@
 package edu.northeastern.cs5520_group9.groupProject;
 
 import android.content.Context;
-import android.media.AudioManager;
+import android.media.AudioAttributes;
 import android.media.SoundPool;
 
 import edu.northeastern.cs5520_group9.R;
-
 
 public class Sound {
 
     private static SoundPool soundPool;
     private static int hitSound;
-    private static int sadSound;
-    private static int happySound;
+    private static int wrongSound;
+    private static int rightSound;
     private static int endSound;
 
 
     public Sound(Context context) {
 
-        //SoundPoll (int maxStreams, int streamType, int srcQuality)
-        soundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
+        // Create the soundPool
+        AudioAttributes attrs = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(4)
+                .setAudioAttributes(attrs)
+                .build();
 
+        // Load hit, wrong, right, end sounds
         hitSound = soundPool.load(context, R.raw.hit, 1);
-        sadSound = soundPool.load(context, R.raw.sad, 1);
-        happySound = soundPool.load(context, R.raw.happy, 1);
+        wrongSound = soundPool.load(context, R.raw.wrong, 1);
+        rightSound = soundPool.load(context, R.raw.right, 1);
         endSound = soundPool.load(context, R.raw.end, 1);
     }
 
+    // Play Hit Sound
     public void playHitSound() {
-        //play(int soundID, float leftVolume, int priority, int loop, float rate)
         soundPool.play(hitSound, 1.0f, 1.0f, 1, 0, 1.0f);
     }
 
-    public void playSadSound() {
-        //play(int soundID, float leftVolume, int priority, int loop, float rate)
-        soundPool.play(sadSound, 1.0f, 1.0f, 1, 0, 1.0f);
+    // Play Wrong Sound
+    public void playWrongSound() {
+        soundPool.play(wrongSound, 1.0f, 1.0f, 1, 0, 1.0f);
     }
 
-    public void playHappySound() {
-        //play(int soundID, float leftVolume, int priority, int loop, float rate)
-        soundPool.play(happySound, 1.0f, 1.0f, 1, 0, 1.0f);
+    // Play Right Sound
+    public void playRightSound() {
+        soundPool.play(rightSound, 1.0f, 1.0f, 1, 0, 1.0f);
     }
 
+    // Play End Sound
     public void playEndSound() {
-        //play(int soundID, float leftVolume, int priority, int loop, float rate)
         soundPool.play(endSound, 1.0f, 1.0f, 1, 0, 1.0f);
     }
 }
